c82b191fcc085e285ea2c6bbbb11ebec60aa058a
Derrick Adjie <d.adjie@comicrelief.com>
2017-01-12 17:35:58 +0000

    variables file

---
+++ variables.tf
@@ -0,0 +1,67 @@
+variable "environment" {
+  default = ""
+}
+
+variable "aws_region" {
+  description = "aws region"
+  default = "eu-west-1"
+}
+
+variable "ami" {
+  default = ""
+}
+
+variable "az" {
+  description = "ec2 availability zone"
+  default = ""
+}
+
+variable "environment" {
+  default = ""
+}
+
+variable "count" {
+  default = ""
+}
+
+variable "key_name" {
+  description = ""
+  default = ""
+}
+
+variable "ami" {
+  default = ""
+}
+
+variable "security_groups" {
+  default = ""
+}
+
+variable "instance_type" {
+  default = ""
+}
+
+variable "subnet_id" {
+  default = ""
+}
+
+
+
+
+
+
+variable "vpc" {
+  default = ""
+}
+
+variable "" {
+  default = ""
+}
+
+variable "public_subnet" {
+  default = ""
+}
+
+variable "private_subnet" {
+  default = ""
+}
