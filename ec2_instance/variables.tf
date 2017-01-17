fe6115b0e1e1dc9764bb0fc2fbac3e326e233624
Derrick Adjie <d.adjie@comicrelief.com>
2017-01-17 10:40:38 +0000

    including new tf files and ec2 folder

---
+++ ec2_instance/variables.tf
@@ -0,0 +1,20 @@
+variable "az" {
+  description = "ec2 availability zone"
+  default = "eu-west-1a"
+}
+
+variable "count" {
+  default = ""
+}
+
+variable "instance_type" {
+  default = ""
+}
+
+variable "ami" {
+  default = ""
+}
+
+variable "security_groups" {
+  default = ""
+}
