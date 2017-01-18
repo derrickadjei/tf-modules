9abf40b99b368a1503f7d068991cf992d5fb0530
Derrick Adjie <d.adjie@comicrelief.com>
2017-01-18 11:56:12 +0000

    code included in to main files

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -1,9 +1,49 @@
-provider "aws" {
-  region = "${var.aws_region}"
+
+
+resource "aws_instance" "name" {
+  #ami = "${var.ami}"
+  availability_zone = "${var.az.a}"
+  count = "${var.web_count}"
+  instance_type = "${var.instance_size}"
+  key_name = "${var.key_name}"
+  security_groups = ["${aws_security_group.name.id}"]
+  subnet_id = "${var.eu-west-1a-private}"
+  tags {
+    Name = "name-${var.name}"
+    Class = "${var.class}"
+    Product = "${var.product}"
+    Env = "${var.environment}"
+  }
 }
 
-module "vpc" {
-  source = ""
-  subnet_id = "${var.private_subnet}"
-  security_groups
+resource "aws_instance" "name" {
+  #ami = "${var.ami}"
+  availability_zone = "${var.az.b}"
+  count = "${var.web_count}"
+  instance_type = "${var.instance_size}"
+  key_name = "${var.key_name}"
+  security_groups = ["${aws_security_group.name.id}"]
+  subnet_id = "${var.eu-west-1a-private}"
+  tags {
+    Name = "name-${var.name}"
+    Class = "${var.class}"
+    Product = "${var.product}"
+    Env = "${var.environment}"
+  }
+}
+
+resource "aws_instance" "name" {
+  #ami = "${var.ami}"
+  availability_zone = "${var.az.c}"
+  count = "${var.web_count}"
+  instance_type = "${var.instance_size}"
+  key_name = "${var.key_name}"
+  security_groups = ["${aws_security_group.name.id}"]
+  subnet_id = "${var.eu-west-1a-private}"
+  tags {
+    Name = "name-${var.name}"
+    Class = "${var.class}"
+    Product = "${var.product}"
+    Env = "${var.environment}"
+  }
 }
