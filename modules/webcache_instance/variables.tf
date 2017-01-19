ccb4236834dea7dcd8ec2b3b584a4b453dfa94c6
Derrick Adjie <d.adjie@comicrelief.com>
2017-01-19 14:19:14 +0000

    modifyinginstance variables file and main

---
+++ modules/webcache_instance/variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+  default = ""
+}
+
+variable "webcache_count" {
+  default = ""
+}
+
+variable "instant_size" {
+  default = ""
+}
+
+variable "aws_key_name" {
+  default = ""
+}
+
+variable "subnet_id" {
+  default = ""
+}
