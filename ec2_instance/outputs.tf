b246f776dd5cf610d7f7d3ff5bdb61fb6428eca5
Derrick Adjie <d.adjie@comicrelief.com>
2017-01-19 11:26:13 +0000

    removing unwanted files

--- ec2_instance/outputs.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "${var.aws_region}"
-}
-
-module "vpc" {
-  source = ""
-  subnet_id = "${var.private_subnet}"
-  security_groups
-}
--- webcache_instance/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "name" { default = ""}
-variable "vpc_id" {}
-variable "vpc_cidr" {}
-variable "secure_cidr" {}
-variable "environment" {}
