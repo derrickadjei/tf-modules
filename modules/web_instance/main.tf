260894c6b4ee736e5b8be8c0d9db2b0662c5e8b2
Derrick Adjie <d.adjie@comicrelief.com>
2017-01-20 17:02:54 +0000

    variables included

--- modules/web_instance/main.tf
+++ modules/web_instance/main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "name" {
   availability_zone = "eu-west-1a"
   count = "${var.webcache_count}"
   instance_type = "${var.instance_size}"
-  key_name = "${var.aws_key_name}"
+  key_name = "${var.key_name}"
   security_groups = ["${module.security_group.security_group_id_web}"]
   subnet_id = "${var.subnet_id}"
   tags {
