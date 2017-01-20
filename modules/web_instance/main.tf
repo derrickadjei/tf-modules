9ed08284f3d5a38d10d18e57ddceb1fab7f7ca35
Derrick Adjie <d.adjie@comicrelief.com>
2017-01-20 15:43:13 +0000

    adding module to web_instance main

--- modules/web_instance/main.tf
+++ modules/web_instance/main.tf
@@ -1,11 +1,15 @@
+module "security_group" {
+  source = "../web_sg"
+}
 
-resource "aws_instance" "rnd17-webcache" {
+
+resource "aws_instance" "name" {
   ami = "${var.ami}"
   availability_zone = "eu-west-1a"
   count = "${var.webcache_count}"
   instance_type = "${var.instance_size}"
   key_name = "${var.aws_key_name}"
-  security_groups = ["${module.modulename.security_group_id_web}"]
+  security_groups = ["${module.security_group.security_group_id_web}"]
   subnet_id = "${var.subnet_id}"
   tags {
     Name = "rnd17-webcache-${var.name}"
