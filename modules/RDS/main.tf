0da0fb56b2bf41d3444b52caa6d71a0a90df8fb7
Derrick Adjie <d.adjie@comicrelief.com>
2017-01-20 10:16:41 +0000

    modifying memcache & RDS main

---
+++ modules/RDS/main.tf
@@ -0,0 +1,79 @@
+resource "aws_db_instance" "rnd17-db" {
+    identifier = "rnd17-db-${var.environment}"
+    allocated_storage = "${consul_keys.env.var.db_storage}"
+    storage_type = "${consul_keys.env.var.db_storage_type}"
+    engine = "mysql"
+    engine_version = "5.6"
+    parameter_group_name = "${aws_db_parameter_group.db.name}"
+    instance_class = "${consul_keys.env.var.db_instance_size}"
+    name = "${consul_keys.env.var.db_name}"
+    username = "${consul_keys.env.var.db_user}"
+    password = "${consul_keys.env.var.db_passwd}"
+    db_subnet_group_name = "${aws_db_subnet_group.db.name}"
+    vpc_security_group_ids = ["${aws_security_group.rnd17-db.id}"]
+}
+
+resource "aws_db_parameter_group" "db" {
+    name = "db-${var.environment}"
+    family = "mysql5.6"
+    description = "RDS default parameter group"
+
+    parameter {
+      name = "character_set_server"
+      value = "utf8"
+    }
+
+    parameter {
+      name = "character_set_client"
+      value = "utf8"
+    }
+}
+
+resource "aws_db_subnet_group" "db" {
+    name = "rnd17-db-subnet-${var.environment}"
+    description = "Subnet group for rnd MySQL db"
+    subnet_ids = ["${consul_keys.env.var.eu-west-1a-private}", "${consul_keys.env.var.eu-west-1b-private}", "${consul_keys.env.var.eu-west-1c-private}"]
+}
+
+resource "aws_route53_record" "rnd17-db" {
+   zone_id = "${var.zoneid}"
+   name = "rnd17-db-${var.environment}.sys.comicrelief.com"
+   type = "CNAME"
+   ttl = "300"
+   records = ["${aws_db_instance.rnd17-db.address}"]
+}
+
+resource "aws_security_group" "rnd17-db" {
+  name = "rnd17-db-${var.environment}"
+  description = "Allow SSH traffic from the internet"
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["${consul_keys.env.var.secure_cidr}","${consul_keys.env.var.mgmt_vpc_cidr}", "${consul_keys.env.var.vpc_cidr}", "${var.cr_lan_ip}"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${consul_keys.env.var.secure_cidr}","${consul_keys.env.var.mgmt_vpc_cidr}", "${consul_keys.env.var.vpc_cidr}", "${var.cr_lan_ip}"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${consul_keys.env.var.vpc_id}"
+  tags {
+    Env = "${var.environment}"
+    Class = "securitygroup"
+  }
+
+}
+
+output "web_count" {
+    value = "${var.web_count}"
+}
---
+++ modules/memcache/main.tf
@@ -0,0 +1,58 @@
+resource "aws_elasticache_cluster" "rnd17-store" {
+    cluster_id = "rnd17-${var.environment}" #cam rather than rnd17 as it can't be longer than 20 chars
+    engine = "memcached"
+    node_type = "${consul_keys.env.var.mc_instance_size}"
+    port = 11211
+    num_cache_nodes = 1
+    parameter_group_name = "default.memcached1.4"
+    security_group_ids = ["${aws_security_group.rnd17-store.id}"]
+    subnet_group_name = "${aws_elasticache_subnet_group.rnd17-store.name}"
+    tags {
+      Name = "rnd17-${var.environment}"
+      Class = "cache"
+      Product = "rnd17"
+      Env = "${var.environment}"
+    }
+}
+
+resource "aws_elasticache_subnet_group" "rnd17-store" {
+    name = "rnd17-store-subnet-${var.environment}"
+    description = "Subnet group for rnd Memcache cluster"
+    subnet_ids = ["${consul_keys.env.var.eu-west-1a-private}","${consul_keys.env.var.eu-west-1b-private}","${consul_keys.env.var.eu-west-1c-private}"]
+}
+
+resource "aws_route53_record" "rnd17-store" {
+   zone_id = "${var.zoneid}"
+   name = "rnd17-store-${var.environment}.sys.comicrelief.com"
+   type = "CNAME"
+   ttl = "300"
+   records = ["${aws_elasticache_cluster.rnd17-store.cache_nodes.0.address}"]
+}
+
+resource "aws_security_group" "rnd17-store" {
+  name = "rnd17-memcache-${var.environment}"
+  description = "Allow SSH traffic from the internet"
+
+  ingress {
+    from_port = 11211
+    to_port = 11211
+    protocol = "tcp"
+    cidr_blocks = ["${consul_keys.env.var.vpc_cidr}"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  vpc_id = "${consul_keys.env.var.vpc_id}"
+  tags {
+    Env = "${var.environment}"
+    Class = "securitygroup"
+  }
+
+}
+
+output "vpc_id" {
+    value = "${consul_keys.env.var.vpc_id}"
+}
