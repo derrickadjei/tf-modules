a1636f30e61e3496735993b04b067051cc1cdb5c
Derrick Adjie <d.adjie@comicrelief.com>
2017-01-23 11:34:56 +0000

    instant type variable default included

--- modules/web_instance/main.tf
+++ modules/web_instance/main.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "name" {
   ami = "${var.ami}"
   availability_zone = "${var.availability_zone}"
   count = "${var.webcache_count}"
-  instance_type = "${var.instance_size}"
+  instance_type = "${var.instance_type}"
   key_name = "${var.key_name}"
   security_groups = ["${module.security_group.security_group_id_web}"]
   subnet_id = "${var.subnet_id}"
