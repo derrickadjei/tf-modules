8b4dd1059f5148e18fb5ed13d5825cb2be1084f3
Derrick Adjie <d.adjie@comicrelief.com>
2017-01-31 12:42:10 +0000

    comment out values

--- modules/web_sg/main.tf
+++ modules/web_sg/main.tf
@@ -1,13 +1,13 @@
 
-module "consul" {
-  source = " ansible-playbook-test/tf/testmain.tf/"
+#module "consul" {
+#  source = " ansible-playbook-test/tf/testmain.tf/"
 
-  vpc_id = "${consul_keys.env.var.vpc_id}"
-  secure_cidr = "${consul_keys.env.var.secure_cidr}"
-  mgmt_vpc_cidr = "${consul_keys.env.var.mgmt_vpc_cidr}"
-  cr_lan_ip = "${var.cr_lan_ip}"
-  vpc_cidr = "${consul_keys.env.var.vpc_cidr}"
-}
+#  vpc_id = "${consul_keys.env.var.vpc_id}"
+#  secure_cidr = "${consul_keys.env.var.secure_cidr}"
+#  mgmt_vpc_cidr = "${consul_keys.env.var.mgmt_vpc_cidr}"
+#  cr_lan_ip = "${var.cr_lan_ip}"
+#  vpc_cidr = "${consul_keys.env.var.vpc_cidr}"
+#}
 
 
 
@@ -25,13 +25,13 @@ resource "aws_security_group" "test-web" {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["${module.consul.var.secure_cidr}","${module.consul.var.mgmt_vpc_cidr}", "${module.consul.var.cr_lan_ip}", "${module.consul.var.vpc_cidr}" ]
+    cidr_blocks = ["${var.secure_cidr}","${var.mgmt_vpc_cidr}", "${var.cr_lan_ip}", "${var.vpc_cidr}" ]
   }
   ingress {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = ["${module.consul.var.secure_cidr}","${module.consul.var.mgmt_vpc_cidr}", "${module.consul.var.vpc_cidr}", "${module.consul.var.cr_lan_ip}"]
+    cidr_blocks = ["${var.secure_cidr}","${var.mgmt_vpc_cidr}", "${var.vpc_cidr}", "${var.cr_lan_ip}"]
   }
   ingress {
     from_port = 24007
@@ -55,13 +55,13 @@ resource "aws_security_group" "test-web" {
     from_port = 8300
     to_port = 8301
     protocol = "tcp"
-    cidr_blocks = ["${module.consul.var.vpc_cidr}"]
+    cidr_blocks = ["${var.vpc_cidr}"]
   }
   ingress {
     from_port = 8300
     to_port = 8301
     protocol = "udp"
-    cidr_blocks = ["${module.consul.var.vpc_cidr}"]
+    cidr_blocks = ["${var.vpc_cidr}"]
   }
   ingress {
     from_port = 2049
