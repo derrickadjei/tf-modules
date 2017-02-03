21afe35be9b8448eb08f2738cd09fc202e6e4beb
Derrick Adjie <d.adjie@comicrelief.com>
2017-02-03 15:49:54 +0000

    changes to elb file

--- modules/elb/main.tf
+++ modules/elb/main.tf
@@ -1,23 +1,54 @@
-resource "aws_elb" "rnd17-elb" {
-  name = "rnd17-elb-${var.environment}"
-  subnets = ["${consul_keys.env.var.eu-west-1a-public}", "${consul_keys.env.var.eu-west-1b-public}", "${consul_keys.env.var.eu-west-1c-public}"]
-  security_groups = ["${aws_security_group.rnd17-elb.id}"]
-  internal = false
+#######
+# ELB #
+#######
 
+resource "aws_security_group" "crab-elb" {
+  name = "crab-elb-${var.environment}"
+  description = "Manage connections to varnish servers"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  vpc_id = "${consul_keys.env.var.vpc_id}"
+  tags {
+    Env = "${var.environment}"
+    Class = "securitygroup"
+  }
+}
+
+resource "aws_elb" "crab-elb" {
+  name = "crab-elb-${var.environment}"
+  subnets = ["${consul_keys.env.var.eu-west-1a-public}", "${consul_keys.env.var.eu-west-1b-public}", "${consul_keys.env.var.eu-west-1c-public}"]
+  security_groups = ["${aws_security_group.crab-elb.id}"]
+  internal = false
   listener {
-    instance_port = 6081
+    instance_port = 80
     instance_protocol = "http"
     lb_port = 80
     lb_protocol = "http"
   }
   listener {
-    instance_port = 6081
+    instance_port = 80
     instance_protocol = "http"
     lb_port = 443
     lb_protocol = "https"
     ssl_certificate_id = "${consul_keys.env.var.ssl_cert_id}"
   }
-
   health_check {
     healthy_threshold = 2
     unhealthy_threshold = 2
@@ -25,17 +56,15 @@ resource "aws_elb" "rnd17-elb" {
     target = "TCP:6081"
     interval = 30
   }
-
-  instances = ["${aws_instance.rnd17-webcache-a.*.id}","${aws_instance.rnd17-webcache-b.*.id}"]
+  instances = ["${aws_instance.crab-web-a.*.id}","${aws_instance.crab-web-b.*.id}","${aws_instance.crab-web-c.*.id}"]
   cross_zone_load_balancing = true
   idle_timeout = 400
   connection_draining = true
   connection_draining_timeout = 400
-
   tags {
-    Name = "rnd17-elb-${var.environment}"
+    Name = "crab-elb-${var.environment}"
     Class = "elb"
-    Product = "rnd17"
+    Product = "crab"
     Env = "${var.environment}"
   }
 }
