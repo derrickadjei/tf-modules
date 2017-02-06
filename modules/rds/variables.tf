53431b8ecb212487bd2ae09fb4c67e473d372705
Derrick Adjie <d.adjie@comicrelief.com>
2017-02-06 16:13:52 +0000

    including variables for rds

---
+++ modules/rds/variables.tf
@@ -0,0 +1,87 @@
+variable "environment" {
+  default = ""
+}
+
+variable "db_storage" {
+  default = ""
+}
+
+variable "db_storage_type" {
+  default = ""
+}
+
+variable "db_instance_size" {
+  default = ""
+}
+
+variable "db_name" {
+  default = ""
+}
+
+variable "db_user" {
+  default = ""
+}
+
+variable "db_passwd" {
+  default = ""
+}
+
+
+variable "secure_cidr" {
+  default = ""
+}
+
+variable "mgmt_vpc_cidr" {
+  default = ""
+}
+
+variable "cr_lan_ip" {
+  default = ""
+}
+
+variable "vpc_cidr" {
+  default = ""
+}
+
+variable "name" {
+  default = ""
+}
+
+variable "aws_ubuntu_ami" {
+  default = ""
+}
+
+variable "web_count" {
+  default = "1"
+}
+
+variable "web-instance_size" {
+  default = ""
+}
+
+variable "aws_key_name" {
+  default = ""
+}
+
+variable "eu-west-1a-private" {
+  default = ""
+}
+
+variable "eu-west-1b-private" {
+  default = ""
+}
+
+variable "eu-west-1c-private" {
+  default = ""
+}
+variable "zoneid" {
+  default = ""
+}
+
+variable "class" {
+  default = ""
+}
+
+variable "product" {
+  default = ""
+}
