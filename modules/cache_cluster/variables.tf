8fedf83020ab7f05a961d88f6bafe678cb4fab6e
Derrick Adjie <d.adjie@comicrelief.com>
2017-02-16 14:47:21 +0000

    variables added to cache_cluster

---
+++ modules/cache_cluster/variables.tf
@@ -0,0 +1,59 @@
+variable "environment" {
+  default = ""
+}
+
+variable "secure_cidr" {
+  default = ""
+}
+
+variable "mgmt_vpc_cidr" {
+  default = ""
+}
+
+variable "cr_lan_ip" {
+  default = ""
+}
+
+variable "vpc_cidr" {
+  default = ""
+}
+
+variable "name" {
+  default = ""
+}
+
+variable "aws_ubuntu_ami" {
+  default = ""
+}
+
+variable "web_count" {
+  default = "1"
+}
+
+variable "web-instance_size" {
+  default = ""
+}
+
+variable "aws_key_name" {
+  default = ""
+}
+
+variable "eu-west-1a-private" {
+  default = ""
+}
+
+variable "eu-west-1b-private" {
+  default = ""
+}
+
+variable "eu-west-1c-private" {
+  default = ""
+}
+
+variable "class" {
+  default = ""
+}
+
+variable "product" {
+  default = ""
+}
